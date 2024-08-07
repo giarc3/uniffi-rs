f2b259bc05eced15e98fa08bfc98e2ce7bafe4f8
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2024-08-07 13:44:02 -0700

    Try reverting oneshot change

--- uniffi_core/src/lib.rs
+++ uniffi_core/src/lib.rs
@@ -42,7 +42,6 @@ pub mod ffi;
 mod ffi_converter_impls;
 mod ffi_converter_traits;
 pub mod metadata;
-mod oneshot;
 
 #[cfg(feature = "scaffolding-ffi-buffer-fns")]
 pub use ffi::ffiserialize::FfiBufferElement;
@@ -61,6 +60,7 @@ pub mod deps {
     pub use async_compat;
     pub use bytes;
     pub use log;
+    pub use oneshot;
     pub use static_assertions;
 }
 
